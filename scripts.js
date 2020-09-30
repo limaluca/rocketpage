ab504d816fdff82104c7150f5ed1c5e090da432e
limaluca <lucaslimamesmo@gmail.com>
2020-09-30 20:12:30 -0300

    added dynamic videos functions

--- scripts.js
+++ scripts.js
@@ -1,13 +1,20 @@
+// selecting an object (a class) called .modalOverlay
 const modalOverlay = document.querySelector('.modalOverlay');
 
+// selecting every object named .card using querySelectorALL
 const cards = document.querySelectorAll('.card');
+
 
 for (let card of cards) {
     card.addEventListener('click', function() {
         modalOverlay.classList.add('active');
+        const videoId = card.getAttribute('id')
+        modalOverlay.querySelector('iframe').src = `https://youtube.com/embed/${videoId}`
     })
 }
 
+// building closing overlay function
 document.querySelector('.closeModal').addEventListener('click', function() {
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe').src = ""
 })
