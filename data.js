15c4f64659f9aabf82ae32d84ca6ed46898ab35a
limaluca <lucaslimamesmo@gmail.com>
2020-10-10 10:26:58 -0300

    Getting data from the back-end to the front-end

---
+++ data.js
@@ -0,0 +1,44 @@
+module.exports = [{
+        id: "axios-um-cliente-http-full-stack",
+        avatar_url: "https://blog.rocketseat.com.br/content/images/2020/09/axios-um-cliente-http-full-stack-rocketseat.png",
+        title: "Axios - um cliente HTTP full Stack",
+        author: "Thiago Marinho",
+        duration: "10 Minutos"
+    },
+    {
+        id: "substituindo-a-instrucao-switch-por-object-literal",
+        avatar_url: "https://blog.rocketseat.com.br/content/images/2020/09/substituindo-a-instrucao-switch-por-object-literal-rocketseat-1.png",
+        title: "Substituindo a instrução switch por Object Literal",
+        author: "Thiago Marinho",
+        duration: "2 Minutos"
+    },
+    {
+        id: "dockerfile-principais-comandos-para-criar-a-receita-da-imagem",
+        avatar_url: "https://blog.rocketseat.com.br/content/images/2020/08/dockerfile-principais-comandos-para-criar-a-receita-da-imagem.png",
+        title: "Dockerfile - Principais comandos para criar a receita",
+        author: "Thiago Marinho",
+        duration: "6 Minutos"
+    }
+]
+
+/*
+<div class="cardImageContainer">
+            <img src="https://blog.rocketseat.com.br/content/images/2020/09/axios-um-cliente-http-full-stack-rocketseat.png" alt="nodeJS">
+        </div>
+
+        <div class="cardContent">
+            <p>Axios - um cliente HTTP ull Stack</p>
+        </div>
+
+        <div class="cardInfo">
+            <div class="author">
+                <p>Thiago Marinho</p>
+            </div>
+
+            <div class="readingTime">
+                <p>10 Minutos</p>
+            </div>
+        </div>
+    </div>
+
+*/
