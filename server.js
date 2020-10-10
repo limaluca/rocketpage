d522e42035aaf384d2055c16d0a807b0d92826bc
limaluca <lucaslimamesmo@gmail.com>
2020-10-10 10:53:32 -0300

    Getting front page data from the backend

--- server.js
+++ server.js
@@ -18,7 +18,13 @@ server.listen(5000, function() {
 
 
 server.get("/", function(req, res) {
-    return res.render('about')
+    const aboutData = {
+        avatar_url: "https://rocketseat.gallerycdn.vsassets.io/extensions/rocketseat/rocketseatreactnative/3.0.1/1588456740326/Microsoft.VisualStudio.Services.Icons.Default",
+        title: "Rocketseat",
+        description: "Evolua rápido como a tecnologia. Junte-se a milhares de devs e acelere na direção dos seus objetivos.",
+        technologies: ["HTML", "Javascript", "React", "React Native"]
+    }
+    return res.render('about', { aboutData })
 })
 
 server.get("/courses", function(req, res) {
